ecea221c40b8e5d85d212aad7b06d2435eec6a84
André Alvim <andre@alvim.net>
2024-04-23 18:46:53 -0300

    fix: api

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,18 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { version } from 'os';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
  
+  @Get()
+  getHello() {
+    return {
+      message: 'Welcome to the Movies API catalog',
+      documentation: 'https://api-movies.alvimdev.tech/docs',
+      version: '1.0.0',
+    }
+  }
 }
